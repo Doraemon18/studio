83f8fda39b112683ba46fdbc3e9bf6e02107793e
Ravi Kishan <ravikishansmp21@gmail.com>
2025-05-12 13:42:41 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" color="#D44638" style="color:#D44638" height="40" width="40" xmlns="http://www.w3.org/2000/svg">
    
    Relevant files:
    
    And change it as follows:
    
    change the background to light white

--- src/components/icons/gmail-icon.tsx
+++ src/components/icons/gmail-icon.tsx
@@ -1,13 +1,16 @@
 
 import { SiGmail } from "react-icons/si";
 import type React from 'react';
+import { cn } from "@/lib/utils";
 
 export interface GmailIconProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function GmailIcon(props: GmailIconProps) {
+  const { className, ...rest } = props;
   return (
-    <div {...props}>
+    <div className={cn("p-1 rounded-md bg-white", className)} {...rest}>
       <SiGmail size={40} color="#D44638" />
     </div>
   );
 }
+
